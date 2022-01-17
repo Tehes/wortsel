a7c6d79bd1a2c9de2e29228b3a04fa10da1c9ad0
Tino Sabetta <tino.sabetta@gmail.com>
2022-01-17 15:03:56 +0100

    first implementation of key typing. Not finished though.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,37 @@ var app = (function() {
     /* --------------------------------------------------------------------------------------------------
     Variables
     ---------------------------------------------------------------------------------------------------*/
-
+    var keyboard = document.querySelector("#keyboard");
+    var backKey = document.querySelectorAll(".input")[0];
+    var enterKey = document.querySelectorAll(".input")[1];
+    var rows = document.querySelectorAll(".row");
+    var activeRow = 0;
 
     /* --------------------------------------------------------------------------------------------------
     functions
     ---------------------------------------------------------------------------------------------------*/
+    function typeKey() {
+        var letters = rows[activeRow].querySelectorAll(".letter");
+        var i = 0;
+
+        while (letters[i].textContent !== "") {
+            i++;
+            if (!letters[i]) {
+                return;
+            }
+        }
+
+
+
+        if (event.target.classList.contains('key')) {
+            letters[i].textContent = event.target.textContent;
+        }
+    }
+
 
     function init() {
         document.addEventListener("touchstart", function() {}, false);
+        keyboard.addEventListener("click", typeKey, false);
     }
 
     /* --------------------------------------------------------------------------------------------------
