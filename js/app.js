75f165b5965a2b2edda1faa1b0c6eb8e7ece8d7d
Tino Sabetta <Tehes@users.noreply.github.com>
2022-01-19 00:27:37 +0100

    added modal for displaying notifications

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var app = (function() {
     ---------------------------------------------------------------------------------------------------*/
     var keyboard = document.querySelector("#keyboard");
     var rows = document.querySelectorAll(".row");
+	var modal = document.querySelector("aside");
     var activeRow = 0;
     var wordList = ["Abart", "Abbau", "Abend", "Abgas", "Abort", "Abruf", "Abtei", "abtun", "Abzug", "Achse", "Acker", "Acryl", "Adieu", "Adler", "adlig", "affig", "affin", "After", "Agave", "Agent", "Ahorn", "Aktie", "aktiv", "Alarm", "Album", "Alias", "Alibi", "Alien", "Allah", "Allee", "Alles", "Alpen", "Alpha", "Altar", "Alter", "Altöl", "Amigo", "Amöbe", "Ampel", "Amsel", "Anbau", "anbei", "Anden", "Angel", "Angst", "Anker", "Anmut", "Anode", "Anruf", "antik", "antun", "Anzug", "Aorta", "Apell", "Apfel", "April", "Arche", "Areal", "Arena", "Armee", "Armut", "Aroma", "Arsch", "artig", "Asien", "Asket", "Aspik", "Assel", "Athen", "Atlas", "atmen", "Atoll", "Audio", "Autor", "Axiom", "Backe", "baden", "banal", "Bande", "bange", "Banjo", "Bares", "Barke", "Baron", "Basar", "Bauch", "bauen", "Bauer", "Beben", "Beere", "Beete", "Beide", "Beige", "Beine", "Belag", "Beleg", "Beruf", "Besen", "Beste", "beten", "Beuge", "Beule", "Beute", "bevor", "Bezug", "Bibel", "Biber", "Biene", "Biere", "Biest", "Binde", "Bingo", "Birke", "Birne", "Bison", "Bitte", "Blank", "Blase", "Blech", "Blick", "blind", "Blitz", "blöde", "Blond", "Blüte", "Blume", "Bluse", "Boden", "Bogen", "Bohne", "Bombe", "Bowle", "Brand", "Braue", "Braun", "Braut", "Bravo", "breit", "Brett", "Brief", "Brise", "Bruch", "Buche", "Bucht", "Bügel", "Bürde", "Büste", "Bulle", "Busch", "Busen", "Capri", "Cello", "Chaos", "Chaot", "Chile", "Chili", "China", "Chips", "Chlor", "circa", "Clown", "Comic", "Couch", "Creme", "Curry", "Dabei", "Dachs", "Dämon", "Dafür", "Daher", "dahin", "Damit", "Dampf", "Danke", "daran", "Darin", "darum", "Datei", "Daten", "Dativ", "Datum", "Dauer", "Davon", "Davor", "debil", "Debüt", "Decke", "Degen", "Deich", "Dekan", "Dekor", "Delle", "Delta", "Demut", "Depot", "devot", "Dicht", "Dicke", "Dildo", "Disco", "Docht", "Dogge", "Dogma", "Dolch", "Döner", "Dosis", "Draht", "Drall", "Drama", "Drang", "Dreck", "Drift", "Droge", "Druck", "Dübel", "Duell", "Dürre", "Duett", "dumpf", "Dunst", "Durch", "Durst", "duzen", "Pferd", "Insel", "Knopf", "Hafer", "Feuer"];
     var solution = wordList[getRndInteger(0, wordList.length - 1)].toLowerCase();
@@ -76,6 +77,11 @@ var app = (function() {
 
         colorizeKeyboard(letters);
         activeRow++;
+		
+		if (activeRow === 6) {
+			modal.textContent = "Leider verloren. Gesucht wurde '"+solution.toUpperCase()+"'.";
+			modal.classList.toggle("hidden");
+		}
     }
 
     function colorizeKeyboard(letters) {
