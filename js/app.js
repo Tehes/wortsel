8b0618f954b1143d67478e2585476ea7b933b25c
Tino Sabetta <tino.sabetta@gmail.com>
2022-01-19 13:23:37 +0100

    more modal messages

--- js/app.js
+++ js/app.js
@@ -4,12 +4,10 @@ var app = (function() {
     ---------------------------------------------------------------------------------------------------*/
     var keyboard = document.querySelector("#keyboard");
     var rows = document.querySelectorAll(".row");
-	var modal = document.querySelector("aside");
+    var modal = document.querySelector("aside");
     var activeRow = 0;
     var wordList = ["Abart", "Abbau", "Abend", "Abgas", "Abort", "Abruf", "Abtei", "abtun", "Abzug", "Achse", "Acker", "Acryl", "Adieu", "Adler", "adlig", "affig", "affin", "After", "Agave", "Agent", "Ahorn", "Aktie", "aktiv", "Alarm", "Album", "Alias", "Alibi", "Alien", "Allah", "Allee", "Alles", "Alpen", "Alpha", "Altar", "Alter", "Altöl", "Amigo", "Amöbe", "Ampel", "Amsel", "Anbau", "anbei", "Anden", "Angel", "Angst", "Anker", "Anmut", "Anode", "Anruf", "antik", "antun", "Anzug", "Aorta", "Apell", "Apfel", "April", "Arche", "Areal", "Arena", "Armee", "Armut", "Aroma", "Arsch", "artig", "Asien", "Asket", "Aspik", "Assel", "Athen", "Atlas", "atmen", "Atoll", "Audio", "Autor", "Axiom", "Backe", "baden", "banal", "Bande", "bange", "Banjo", "Bares", "Barke", "Baron", "Basar", "Bauch", "bauen", "Bauer", "Beben", "Beere", "Beete", "Beide", "Beige", "Beine", "Belag", "Beleg", "Beruf", "Besen", "Beste", "beten", "Beuge", "Beule", "Beute", "bevor", "Bezug", "Bibel", "Biber", "Biene", "Biere", "Biest", "Binde", "Bingo", "Birke", "Birne", "Bison", "Bitte", "Blank", "Blase", "Blech", "Blick", "blind", "Blitz", "blöde", "Blond", "Blüte", "Blume", "Bluse", "Boden", "Bogen", "Bohne", "Bombe", "Bowle", "Brand", "Braue", "Braun", "Braut", "Bravo", "breit", "Brett", "Brief", "Brise", "Bruch", "Buche", "Bucht", "Bügel", "Bürde", "Büste", "Bulle", "Busch", "Busen", "Capri", "Cello", "Chaos", "Chaot", "Chile", "Chili", "China", "Chips", "Chlor", "circa", "Clown", "Comic", "Couch", "Creme", "Curry", "Dabei", "Dachs", "Dämon", "Dafür", "Daher", "dahin", "Damit", "Dampf", "Danke", "daran", "Darin", "darum", "Datei", "Daten", "Dativ", "Datum", "Dauer", "Davon", "Davor", "debil", "Debüt", "Decke", "Degen", "Deich", "Dekan", "Dekor", "Delle", "Delta", "Demut", "Depot", "devot", "Dicht", "Dicke", "Dildo", "Disco", "Docht", "Dogge", "Dogma", "Dolch", "Döner", "Dosis", "Draht", "Drall", "Drama", "Drang", "Dreck", "Drift", "Droge", "Druck", "Dübel", "Duell", "Dürre", "Duett", "dumpf", "Dunst", "Durch", "Durst", "duzen", "Ebene", "ebnen", "Ebola", "Echse", "eckig", "Effet", "ehren", "Eiche", "Eifer", "eilig", "Eimer", "einig", "Eisen", "eisig", "eitel", "Eiter", "ekeln", "eklig", "Elend", "emsig", "enden", "Engel", "Enkel", "enorm", "Enzym", "erben", "Erbse", "erdig", "Erdöl", "Erlös", "Ernst", "Ernte", "Erpel", "Esche", "Essay", "Essen", "Essig", "Etage", "Ethik", "Etwas", "Euter", "Event", "exakt", "Extra", "Pferd", "Insel", "Knopf", "Hafer", "Feuer"];
     var solution = wordList[getRndInteger(0, wordList.length - 1)].toLowerCase();
-    console.log(solution);
-    console.log(wordList.length);
 
     /* --------------------------------------------------------------------------------------------------
     functions
@@ -28,6 +26,7 @@ var app = (function() {
             i++;
         }
 
+        // WHEN BACK-BUTTON IS PRESSED
         if (event.target.textContent === "back") {
             if (!letters[i - 1]) {
                 return;
@@ -35,18 +34,25 @@ var app = (function() {
             letters[i - 1].textContent = "";
         }
 
+        // WHEN ENTER-BUTTON IS PRESSED
         else if (event.target.textContent === "enter") {
             if (i === letters.length) {
-                checkSolution(letters);
+                colorizeRow(letters);
+                colorizeKeyboard(letters);
+                hasEnded();
+            }
+            else {
+                showModal("Zu wenig Zeichen", 1000);
             }
         }
 
+        // WHEN ANY LETTER IS PRESSED
         else if (i < letters.length && event.target.classList.contains('key')) {
             letters[i].textContent = event.target.textContent;
         }
     }
 
-    function checkSolution(letters) {
+    function colorizeRow(letters) {
         var i, j, tempSolution;
 
         i = 0;
@@ -74,9 +80,6 @@ var app = (function() {
                 letters[i].classList.add("absent");
             }
         }
-
-        colorizeKeyboard(letters);
-		hasEnded();
     }
 
     function colorizeKeyboard(letters) {
@@ -107,36 +110,55 @@ var app = (function() {
             }
         }
     }
-	
-	function hasEnded() {
-		var correctLetters;
-		
-		correctLetters = rows[activeRow].querySelectorAll(".correct");
-		if (correctLetters.length === 5) {
-			modal.textContent = "Yay, gewonnen!";
-			modal.classList.toggle("hidden");
-		}
-		else {
-			activeRow++;
-		}
-		
-		if (activeRow === 6) {
-			modal.textContent = "Leider verloren. Gesucht wurde '"+solution.toUpperCase()+"'.";
-			modal.classList.toggle("hidden");
-		}
-	}
 
+    function hasEnded() {
+        var correctLetters;
+        correctLetters = rows[activeRow].querySelectorAll(".correct");
+
+        if (correctLetters.length === 5) {
+            modal.textContent = "Yay, gewonnen!";
+            modal.classList.toggle("hidden");
+        }
+        else {
+            activeRow++;
+        }
+
+        if (activeRow === 6) {
+            showModal("Leider verloren. Gesucht wurde '" + solution.toUpperCase() + "'.");
+        }
+    }
+
+    function showModal(text, duration) {
+        modal.textContent = text;
+        modal.classList.toggle("hidden");
+
+        if (duration > 0) {
+            setTimeout(function() {
+                modal.classList.toggle("hidden");
+            }, duration);
+        }
+    }
+
+    function solve() {
+        showModal("Die Lösung lautet '" + solution.toUpperCase() + "'.", 2000);
+    }
 
     function init() {
         document.addEventListener("touchstart", function() {}, false);
+        document.addEventListener("dblclick", function(event) {
+            event.preventDefault();
+        });
         keyboard.addEventListener("click", typeKey, false);
+
+        console.log("amount of words: " + wordList.length);
     }
 
     /* --------------------------------------------------------------------------------------------------
     public members, exposed with return statement
     ---------------------------------------------------------------------------------------------------*/
     return {
-        init: init
+        init: init,
+        solve: solve
     };
 
 })();
