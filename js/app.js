9c1501ab63aabe03fe59acd447ff957b7fbc3ed3
Tino Sabetta <tino.sabetta@gmail.com>
2022-01-24 11:53:20 +0100

    clean up and updated database

--- js/app.js
+++ js/app.js
@@ -1,222 +1,220 @@
-import { curatedWords, additionalWords } from '/js/words.js';
+/* --------------------------------------------------------------------------------------------------
+Variables
+---------------------------------------------------------------------------------------------------*/
+import {
+    curatedWords,
+    additionalWords
+}
+from '/js/words.js';
 
-var app = (function() {
-    /* --------------------------------------------------------------------------------------------------
-    Variables
-    ---------------------------------------------------------------------------------------------------*/
-    var keyboard = document.querySelector("#keyboard");
-    var rows = document.querySelectorAll(".row");
-    var modal = document.querySelector("aside.modal");
-    var howTo = document.querySelector("#howto");
-    var activeRow = 0;
-    var enteredWord = "";
-    var firstVisit = localStorage.getItem("wortsel_firstVisit") || true;
-    var wordList = curatedWords.concat(additionalWords);
-    var solution = curatedWords[getRndInteger(0, curatedWords.length - 1)].toLowerCase();
+var keyboard = document.querySelector("#keyboard");
+var rows = document.querySelectorAll(".row");
+var modal = document.querySelector("aside.modal");
+var howTo = document.querySelector("#howto");
+var activeRow = 0;
+var enteredWord = "";
+var firstVisit = localStorage.getItem("wortsel_firstVisit") || true;
+var wordList = curatedWords.concat(additionalWords);
+var solution = curatedWords[getRndInteger(0, curatedWords.length - 1)].toLowerCase();
 
-    /* --------------------------------------------------------------------------------------------------
-    functions
-    ---------------------------------------------------------------------------------------------------*/
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+/* --------------------------------------------------------------------------------------------------
+functions
+---------------------------------------------------------------------------------------------------*/
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function typeKey() {
+    var letters, i;
+
+    letters = rows[activeRow].querySelectorAll(".letter");
+    i = 0;
+
+    while (i < letters.length && letters[i].textContent !== "") {
+        i++;
     }
 
-    function typeKey() {
-        var letters, i;
-
-        letters = rows[activeRow].querySelectorAll(".letter");
-        i = 0;
-
-        while (i < letters.length && letters[i].textContent !== "") {
-            i++;
+    // WHEN BACK-BUTTON IS PRESSED
+    if (event.target.textContent === "back") {
+        if (!letters[i - 1]) {
+            return;
         }
+        letters[i - 1].textContent = "";
+    }
 
-        // WHEN BACK-BUTTON IS PRESSED
-        if (event.target.textContent === "back") {
-            if (!letters[i - 1]) {
-                return;
-            }
-            letters[i - 1].textContent = "";
-        }
-
-        // WHEN ENTER-BUTTON IS PRESSED
-        else if (event.target.textContent === "enter") {
-            if (i === letters.length) {
-                if (indexInDatabase(letters) !== -1) {
-					colorizeRow(letters);
-                	colorizeKeyboard(letters);
-                	hasEnded();
-                }
-                else {
-					playErrorAnimation();
-					showModal("Kein zulässiges Wort", 1000);
-                }
+    // WHEN ENTER-BUTTON IS PRESSED
+    else if (event.target.textContent === "enter") {
+        if (i === letters.length) {
+            if (indexInDatabase(letters) !== -1) {
+                colorizeRow(letters);
+                colorizeKeyboard(letters);
+                hasEnded();
             }
             else {
-                showModal("Zu wenig Zeichen", 1000);
+                playErrorAnimation();
+                showModal("Kein zulässiges Wort", 1000);
             }
-        }
-
-        // WHEN ANY LETTER IS PRESSED
-        else if (i < letters.length && event.target.classList.contains('key')) {
-            letters[i].textContent = event.target.textContent;
-        }
-    }
-
-	function indexInDatabase(letters) {
-		var index;
-
-		enteredWord = [...letters].map(letters => letters.textContent);
-        enteredWord = enteredWord.toString();
-        enteredWord = enteredWord.replace(/,/g, "");
-
-        index = wordList.findIndex(element => {
-        	return element.toLowerCase() === enteredWord.toLowerCase();
-		});
-
-		return index;
-	}
-
-    function colorizeRow(letters) {
-        var i, j, tempSolution;
-
-        i = 0;
-        tempSolution = solution.split("");
-
-        for (i = 0; i < letters.length; i++) {
-            if (letters[i].textContent === solution[i]) {
-                letters[i].classList.add("correct");
-                tempSolution[i] = "";
-            }
-        }
-
-        for (i = 0; i < letters.length; i++) {
-            if (!letters[i].classList.contains("correct")) {
-                j = tempSolution.indexOf(letters[i].textContent);
-                if (j !== -1) {
-                    letters[i].classList.add("present");
-                    tempSolution[j] = "";
-                }
-            }
-        }
-
-        for (i = 0; i < letters.length; i++) {
-            if (!letters[i].classList.contains("correct") && !letters[i].classList.contains("present")) {
-                letters[i].classList.add("absent");
-            }
-        }
-    }
-
-    function colorizeKeyboard(letters) {
-        var i, j, keys, arrkeys;
-        i = 0;
-        keys = document.querySelectorAll(".key");
-        arrkeys = [...keys].map(keys => keys.textContent);
-
-        for (i = 0; i < letters.length; i++) {
-            j = arrkeys.indexOf(letters[i].textContent);
-
-            if (letters[i].classList.contains("absent") &&
-                !keys[j].classList.contains("correct") &&
-                !keys[j].classList.contains("present")) {
-                keys[j].classList.add("absent");
-            }
-
-            if (letters[i].classList.contains("present") &&
-                !keys[j].classList.contains("correct")) {
-                keys[j].classList.add("present");
-                keys[j].classList.remove("absent");
-            }
-
-            if (letters[i].classList.contains("correct")) {
-                keys[j].classList.add("correct");
-                keys[j].classList.remove("present");
-                keys[j].classList.remove("absent");
-            }
-        }
-    }
-
-    function hasEnded() {
-        var correctLetters;
-        correctLetters = rows[activeRow].querySelectorAll(".correct");
-
-        if (correctLetters.length === 5) {
-            showModal("Yay, gewonnen!", 3000);
-            playWinAnimation(correctLetters);
         }
         else {
-            activeRow++;
-        }
-
-        if (activeRow === 6) {
-            showModal("Leider verloren. Gesucht wurde '" + solution.toUpperCase() + "'.", 3000);
+            showModal("Zu wenig Zeichen", 1000);
         }
     }
 
-    function showModal(text, duration) {
-        modal.textContent = text;
-        modal.classList.remove("hidden");
+    // WHEN ANY LETTER IS PRESSED
+    else if (i < letters.length && event.target.classList.contains('key')) {
+        letters[i].textContent = event.target.textContent;
+    }
+}
 
-        if (duration > 0) {
-            setTimeout(function() {
-                modal.classList.add("hidden");
-            }, duration);
+function indexInDatabase(letters) {
+    var index;
+
+    enteredWord = [...letters].map(letters => letters.textContent);
+    enteredWord = enteredWord.toString();
+    enteredWord = enteredWord.replace(/,/g, "");
+
+    index = wordList.findIndex(element => {
+        return element.toLowerCase() === enteredWord.toLowerCase();
+    });
+
+    return index;
+}
+
+function colorizeRow(letters) {
+    var i, j, tempSolution;
+
+    i = 0;
+    tempSolution = solution.split("");
+
+    for (i = 0; i < letters.length; i++) {
+        if (letters[i].textContent === solution[i]) {
+            letters[i].classList.add("correct");
+            tempSolution[i] = "";
         }
     }
 
-    function hideWindow() {
-        event.currentTarget.classList.add("hidden");
-    }
-
-    function playWinAnimation(letters) {
-        var i;
-
-        for (i = 0; i < letters.length; i++) {
-            letters[i].classList.add("jump");
+    for (i = 0; i < letters.length; i++) {
+        if (!letters[i].classList.contains("correct")) {
+            j = tempSolution.indexOf(letters[i].textContent);
+            if (j !== -1) {
+                letters[i].classList.add("present");
+                tempSolution[j] = "";
+            }
         }
     }
 
-	function playErrorAnimation() {
-		rows[activeRow].classList.add("shake");
+    for (i = 0; i < letters.length; i++) {
+        if (!letters[i].classList.contains("correct") && !letters[i].classList.contains("present")) {
+            letters[i].classList.add("absent");
+        }
+    }
+}
+
+function colorizeKeyboard(letters) {
+    var i, j, keys, arrkeys;
+    i = 0;
+    keys = document.querySelectorAll(".key");
+    arrkeys = [...keys].map(keys => keys.textContent);
+
+    for (i = 0; i < letters.length; i++) {
+        j = arrkeys.indexOf(letters[i].textContent);
+
+        if (letters[i].classList.contains("absent") &&
+            !keys[j].classList.contains("correct") &&
+            !keys[j].classList.contains("present")) {
+            keys[j].classList.add("absent");
+        }
+
+        if (letters[i].classList.contains("present") &&
+            !keys[j].classList.contains("correct")) {
+            keys[j].classList.add("present");
+            keys[j].classList.remove("absent");
+        }
+
+        if (letters[i].classList.contains("correct")) {
+            keys[j].classList.add("correct");
+            keys[j].classList.remove("present");
+            keys[j].classList.remove("absent");
+        }
+    }
+}
+
+function hasEnded() {
+    var correctLetters;
+    correctLetters = rows[activeRow].querySelectorAll(".correct");
+
+    if (correctLetters.length === 5) {
+        showModal("Yay, gewonnen!", 3000);
+        playWinAnimation(correctLetters);
+    }
+    else {
+        activeRow++;
     }
 
-    function stopAnyAnimation() {
-        event.target.classList.remove("jump");
-		event.target.classList.remove("shake");
+    if (activeRow === 6) {
+        showModal("Leider verloren. Gesucht wurde '" + solution.toUpperCase() + "'.", 3000);
+    }
+}
+
+function showModal(text, duration) {
+    modal.textContent = text;
+    modal.classList.remove("hidden");
+
+    if (duration > 0) {
+        setTimeout(function() {
+            modal.classList.add("hidden");
+        }, duration);
+    }
+}
+
+function hideWindow() {
+    event.currentTarget.classList.add("hidden");
+}
+
+function playWinAnimation(letters) {
+    var i;
+
+    for (i = 0; i < letters.length; i++) {
+        letters[i].classList.add("jump");
+    }
+}
+
+function playErrorAnimation() {
+    rows[activeRow].classList.add("shake");
+}
+
+function stopAnyAnimation() {
+    event.target.classList.remove("jump");
+    event.target.classList.remove("shake");
+}
+
+function solve() {
+    showModal("Die Lösung lautet '" + solution.toUpperCase() + "'.", 2000);
+}
+
+function saveSettings() {
+    localStorage.setItem("wortsel_firstVisit", false);
+}
+
+function init() {
+    if (firstVisit === true) {
+        howTo.classList.remove("hidden");
     }
 
-    function solve() {
-        showModal("Die Lösung lautet '" + solution.toUpperCase() + "'.", 2000);
-    }
+    var gameBoard = document.querySelector("main");
 
-    function saveSettings() {
-        localStorage.setItem("wortsel_firstVisit", false);
-    }
+    document.addEventListener("touchstart", function() {}, false);
+    gameBoard.addEventListener("animationend", stopAnyAnimation, false);
+    keyboard.addEventListener("click", typeKey, false);
+    howTo.addEventListener("click", hideWindow, false);
+    window.addEventListener("unload", saveSettings, false);
 
-    function init() {
-        if (firstVisit === true) { howTo.classList.remove("hidden"); }
+    console.log("curated words: " + curatedWords.length);
+    console.log("additional words: " + additionalWords.length);
+    console.log("altogether: " + wordList.length);
+}
 
-        var gameBoard = document.querySelector("main");
-        
-        document.addEventListener("touchstart", function() {}, false);
-        gameBoard.addEventListener("animationend", stopAnyAnimation, false);
-        keyboard.addEventListener("click", typeKey, false);
-        howTo.addEventListener("click", hideWindow, false);
-        window.addEventListener("unload", saveSettings, false);
-
-        console.log("curated words: " + curatedWords.length);
-        console.log("additional words: "+ additionalWords.length);
-        console.log("altogether: "+wordList.length);
-    }
-
-    /* --------------------------------------------------------------------------------------------------
-    public members, exposed with return statement
-    ---------------------------------------------------------------------------------------------------*/
-    return {
-        init: init,
-        solve: solve
-    };
-
-})();
-
-app.init();
+/* --------------------------------------------------------------------------------------------------
+public members, exposed with return statement
+---------------------------------------------------------------------------------------------------*/
+init();
