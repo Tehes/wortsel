9a01fd5813e0d4b88dcdf71156e8a588174475e8
Tino Sabetta <tino.sabetta@gmail.com>
2025-08-25 13:48:07 +0200

    updated service worker

--- js/app.js
+++ js/app.js
@@ -605,7 +605,7 @@ globalThis.wortsel.initGame();
 Service Worker configuration. Toggle 'useServiceWorker' to enable or disable the Service Worker.
 ---------------------------------------------------------------------------------------------------*/
 const useServiceWorker = true; // Set to "true" if you want to register the Service Worker, "false" to unregister
-const serviceWorkerVersion = "2025-08-24-v1"; // Increment this version to force browsers to fetch a new service-worker.js
+const serviceWorkerVersion = "2025-08-25-v1"; // Increment this version to force browsers to fetch a new service-worker.js
 
 async function registerServiceWorker() {
 	try {
