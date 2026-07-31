d418ea6e6ef92f1688f0fdd2114a7f07c95d5c2d
Tino Sabetta <tino.sabetta@gmail.com>
2026-07-31 13:09:10 +0200

    Use local fallback sources for F.A.Z. fonts

--- js/app.js
+++ js/app.js
@@ -1289,7 +1289,7 @@ globalThis.wortsel.initGame();
  * - AUTO_RELOAD_ON_SW_UPDATE: reload page once after an update
  -------------------------------------------------------------------------------------------------- */
 const USE_SERVICE_WORKER = true;
-const SERVICE_WORKER_VERSION = "2026-07-31-v9";
+const SERVICE_WORKER_VERSION = "2026-07-31-v10";
 const AUTO_RELOAD_ON_SW_UPDATE = false;
 
 /* --------------------------------------------------------------------------------------------------
