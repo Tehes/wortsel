25a47a733a468619c486d087116cba698046916b
Tino Sabetta <tino.sabetta@gmail.com>
2026-08-28 15:02:11 +0200

    quick fix for empty history state

--- js/app.js
+++ js/app.js
@@ -1632,7 +1632,7 @@ globalThis.wortsel.initGame();
  * - AUTO_RELOAD_ON_SW_UPDATE: reload page once after an update
  -------------------------------------------------------------------------------------------------- */
 const USE_SERVICE_WORKER = true;
-const SERVICE_WORKER_VERSION = "2026-08-28-v21";
+const SERVICE_WORKER_VERSION = "2026-08-28-v22";
 const AUTO_RELOAD_ON_SW_UPDATE = false;
 
 /* --------------------------------------------------------------------------------------------------
