73af380175c662559ffecab80755341893836071
Tino Sabetta <tino.sabetta@gmail.com>
2026-09-07 15:30:46 +0200

    updated additional words

--- js/app.js
+++ js/app.js
@@ -1628,7 +1628,7 @@ globalThis.wortsel.initGame();
  * - AUTO_RELOAD_ON_SW_UPDATE: reload page once after an update
  -------------------------------------------------------------------------------------------------- */
 const USE_SERVICE_WORKER = true;
-const SERVICE_WORKER_VERSION = "2026-08-28-v23";
+const SERVICE_WORKER_VERSION = "2026-09-07-v1";
 const AUTO_RELOAD_ON_SW_UPDATE = false;
 
 /* --------------------------------------------------------------------------------------------------
